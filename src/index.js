36ad5e8bbb5a4fb5cb3c204ab23ddec632c97f64
isaac-michel224 <isaac.michel224@gmail.com>
2021-09-13 10:30:35 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import StateCounter from './StateCounter';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import ReduxCounter from './ReduxCounter';
+import { createStore } from 'redux';
+
+
+const initialState = {
+  count: 0
+}
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+    return{
+      count: state.count + 1
+    };
+    case 'DECREMENT':
+    return{
+      count: state.count - 1
+    };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ReduxCounter />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
